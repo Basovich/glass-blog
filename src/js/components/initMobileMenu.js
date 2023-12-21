bee874dfa99b7699d269f9335202176c4c9eb31b
vadim <vadim.yakovenko86@gmail.com>
2023-12-21 12:35:53 +0200

    added header + search form

--- src/js/components/initMobileMenu.js
+++ src/js/components/initMobileMenu.js
@@ -1,13 +1,27 @@
 export const initMobileMenu = () => {
   // VARS
   const hamburger = document.querySelector('[data-hamburger]');
+  const btnOpenSearchForm = document.querySelector('[data-open-search-form]');
+  const btnCloseSearchForm = document.querySelector('[data-close-search-form]');
   const activeMobileMenuClass = 'mobile-menu-is-open';
+  const activeSearchFormClass = 'search-form-is-open';
 
   // LISTENERS
-  hamburger.addEventListener('click', handleOnClickHamburger);
+  hamburger?.addEventListener('click', handleOnClickHamburger);
+  btnOpenSearchForm?.addEventListener('click', handleOnClickOpenSearchForm);
+  btnCloseSearchForm?.addEventListener('click', handleOnClickCloseSearchForm);
 
   // HANDLERS
   function handleOnClickHamburger() {
     document.body.classList.toggle(activeMobileMenuClass);
+  }
+
+  function handleOnClickOpenSearchForm() {
+    document.body.classList.remove(activeMobileMenuClass);
+    document.body.classList.add(activeSearchFormClass);
+  }
+
+  function handleOnClickCloseSearchForm() {
+    document.body.classList.remove(activeSearchFormClass);
   }
 }
